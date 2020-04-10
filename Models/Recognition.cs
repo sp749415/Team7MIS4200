2f40ab64d653213479a684f9868d1cbe2d11139b
Sarah Paukovits <sp749415@ohio.edu>
2020-04-09 23:32:35 -0400

    added recognition model (still no controller)

---
+++ Models/Recognition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team7MIS4200.Models
+{
+    public class Recognition
+    {
+        [Key]
+        public int recId { get; set; }
+
+        [Display(Name= "Core Value Recognized")]
+        [Required]
+        public CoreValue award { get; set; }
+
+        [StringLength(500)]
+        [Required]
+        public string recognition  { get; set; }
+
+        public enum CoreValue
+        {
+            corevalue = 1,
+        }
+        
+    }
+}
